3c8b534036d927ae577b6e47e1c838574bfff5b5
pallab <pallab.115045@gmail.com>
2022-09-28 11:44:48 +0530

    "initial commit"

---
+++ src/test/kotlin/com/org/notes/NotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.org.notes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
